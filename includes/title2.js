3d234e22bc3d54d50097d1a6f753b30fd50c706f
Rahat Ibn Rafiq <rahatibnrafiq@Rahats-MacBook-Pro.local>
2019-01-20 10:30:56 -0700

    final date for the workshop

--- includes/title2.js
+++ includes/title2.js
@@ -9,7 +9,7 @@ document.write("<td align=\"center\">");
 document.write("<h1>CyberSafety 2019</h1>");
 document.write("<h3>The Fourth Workshop on Computational Methods in Online Misbehavior</h3>");
 document.write("<h3>Co-located with <a target=\"_tab\" href=\"https://www2019.thewebconf.org/\" >The Web Conference 2019 </a></h3>");
-document.write("<h3>May 13 or 14, 2019</h3>");
+document.write("<h3>May 13, 2019</h3>");
 document.write("<h3>San Francisco, California, USA</h3>");
 
 document.write("</td>");
